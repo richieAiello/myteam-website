2b4faeaa064d1f3634cce70875cf3fccf3e8d287
Richard Aiello <richaiello17@gmail.com>
2022-08-26 07:25:55 -0400

    Styles for Build.jsx on mobile.

--- src/components/home/build-section/Build.jsx
+++ src/components/home/build-section/Build.jsx
@@ -2,11 +2,12 @@ import BuildFeature from './BuildFeature';
 import person from '../../../assets/icons/icon-person.svg';
 import cog from '../../../assets/icons/icon-cog.svg';
 import chart from '../../../assets/icons/icon-chart.svg';
+import pattern from '../../../assets/bg-patterns/bg-pattern-home-3.svg';
 
 const Build = props => {
   return (
     <div className="bg-state-green py-16">
-      <section className="container before:bg-coral before:w-[3.125rem] before:h-1 before:block">
+      <section className="container relative z-0 before:bg-coral before:w-[3.125rem] before:h-1 before:block">
         <h2 className="heading max-w-[15rem] mt-8 mb-14">
           Build & manage distributed teams like no one else.
         </h2>
@@ -31,6 +32,11 @@ const Build = props => {
             distributed teams."
           />
         </div>
+        <img
+          src={pattern}
+          alt=""
+          className="absolute -top-16 -right-[7.75rem] -z-10"
+        />
       </section>
     </div>
   );
