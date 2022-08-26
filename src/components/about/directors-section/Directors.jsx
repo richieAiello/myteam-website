6f1391baaec0be5770558056965ada7216eac4b4
Richard Aiello <richaiello17@gmail.com>
2022-08-26 10:34:09 -0400

    Added data to directorsData.js. Styles and jsx for DirectorsGrid.jsx on mobile.

--- src/components/about/directors-section/Directors.jsx
+++ src/components/about/directors-section/Directors.jsx
@@ -2,7 +2,7 @@ import DirectorsGrid from './DirectorsGrid';
 
 const Directors = props => {
   return (
-    <div className="bg-jungle-green py-[5.5rem] overflow-hidden">
+    <div className="bg-jungle-green pt-[5.5rem] pb-[7.25rem] overflow-hidden">
       <section className="container relative z-0 text-center">
         <h2 className="heading mb-12">Meet the directors</h2>
         <DirectorsGrid />
--- src/components/about/directors-section/DirectorsGrid.jsx
+++ src/components/about/directors-section/DirectorsGrid.jsx
@@ -5,8 +5,20 @@ const DirectorsGrid = props => {
   const data = directorsData;
 
   return (
-    <div>
-      <DirectorsCard />
+    <div className="grid gap-y-[3.25rem]">
+      {data.map(item => {
+        return (
+          <DirectorsCard
+            key={item.name}
+            image={item.image}
+            name={item.name}
+            position={item.position}
+            quote={item.quote}
+            twitter={item.twitter}
+            linkedin={item.linkedin}
+          />
+        );
+      })}
     </div>
   );
 };
