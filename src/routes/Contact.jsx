e056623f02b9ec2b7d358b47fede4a7194aee659
Richard Aiello <richaiello17@gmail.com>
2022-08-26 12:41:12 -0400

    Styles and jsx for HeroContact.jsx on mobile.

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,13 @@
+import HeroContact from '../components/contact/hero-section/HeroContact';
+
 const Contact = props => {
-  return <main></main>;
+  return (
+    <main className="bg-midnight-green pt-40">
+      <div className="container">
+        <HeroContact />
+      </div>
+    </main>
+  );
 };
 
 export default Contact;
