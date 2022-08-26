9fe47c35e352e625361dee8e858af239bb4b3866
Richard Aiello <richaiello17@gmail.com>
2022-08-26 12:06:19 -0400

    Added bg patterns to Directors.jsx.

--- src/components/about/directors-section/Directors.jsx
+++ src/components/about/directors-section/Directors.jsx
@@ -1,4 +1,6 @@
 import DirectorsGrid from './DirectorsGrid';
+import patternTop from '../../../assets/bg-patterns/bg-pattern-about-2-contact-1.svg';
+import patternBottom from '../../../assets/bg-patterns/bg-pattern-home-4-about-3.svg';
 
 const Directors = props => {
   return (
@@ -6,6 +8,16 @@ const Directors = props => {
       <section className="container relative z-0 text-center">
         <h2 className="heading mb-12">Meet the directors</h2>
         <DirectorsGrid />
+        <img
+          src={patternTop}
+          alt=""
+          className="absolute -z-10 -top-[11.75rem] -left-[7.75rem]"
+        />
+        <img
+          src={patternBottom}
+          alt=""
+          className="absolute -z-10 -bottom-[7.25rem] -right-6"
+        />
       </section>
     </div>
   );
