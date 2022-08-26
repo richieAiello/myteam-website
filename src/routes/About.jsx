8f4ac0b399739a996b4bd991fb23ba8b6d8f4f8c
Richard Aiello <richaiello17@gmail.com>
2022-08-26 09:29:34 -0400

    Styles and jsx for Clients.jsx on mobile.

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,10 +1,12 @@
 import HeroAbout from '../components/about/hero-section/HeroAbout';
+import Clients from '../components/about/clients-section/Clients';
 import Ready from '../components/shared-components/Ready';
 
 const About = props => {
   return (
     <main>
       <HeroAbout />
+      <Clients />
       <Ready />
     </main>
   );
