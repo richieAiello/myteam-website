3590f6d097450e19aeb404a156d7eee645b39d4f
Richard Aiello <richaiello17@gmail.com>
2022-08-27 10:25:17 -0400

    Styles for Contact.jsx on tablet.

--- src/components/contact/hero-section/HeroContact.jsx
+++ src/components/contact/hero-section/HeroContact.jsx
@@ -5,28 +5,28 @@ import chart from '../../../assets/icons/icon-chart.svg';
 const HeroContact = props => {
   return (
     <section className="">
-      <h1 className="heading--hero--secondary text-center mb-4">
+      <h1 className="heading--hero--secondary text-center mb-4 md:mb-6">
         Contact
       </h1>
-      <h2 className="text-coral text-[2rem] leading-[3rem] text-center font-bold mb-10">
+      <h2 className="text-coral text-[2rem] leading-[3rem] text-center font-bold mb-10 md:mb-6">
         Ask us about
       </h2>
-      <div className="grid gap-y-6">
+      <div className="grid gap-y-6 md:gap-y-2 md:max-w-[475px] md:mx-auto">
         <div className="flex items-center justify-start">
           <img src={person} alt="" className="mr-6" />
-          <p className="heading--sub max-w-[22ch]">
+          <p className="heading--sub max-w-[22ch] md:max-w-full">
             The quality of our talent network
           </p>
         </div>
         <div className="flex items-center">
           <img src={cog} alt="" className="mr-6" />
-          <p className="heading--sub max-w-[22ch]">
+          <p className="heading--sub max-w-[22ch] md:max-w-full">
             Usage & implementation of our software
           </p>
         </div>
         <div className="flex items-center">
           <img src={chart} alt="" className="mr-6" />
-          <p className="heading--sub max-w-[22ch]">
+          <p className="heading--sub max-w-[22ch] md:max-w-full">
             How we help drive innovation
           </p>
         </div>
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -5,19 +5,21 @@ import patternBottom from '../assets/bg-patterns/bg-pattern-contact-2.svg';
 
 const Contact = props => {
   return (
-    <main className="bg-midnight-green pt-40 pb-[5.5rem] overflow-hidden">
-      <div className="container relative z-0 grid gap-y-14">
+    <main className="bg-midnight-green pt-40 pb-[5.5rem] overflow-hidden md:pt-56 md:pb-28">
+      <div className="container--secondary relative z-0 grid gap-y-14 md:gap-y-16">
         <HeroContact />
         <ContactForm />
         <img
           src={patternTop}
           alt=""
-          className="hidden absolute -z-10 md:block"
+          className="hidden absolute -z-10 md:block md:-left-[12.375rem] md:top-0"
+          loading="lazy"
         />
         <img
           src={patternBottom}
           alt=""
-          className="absolute -z-10 -right-[7.75rem] -bottom-[12.5rem]"
+          className="absolute -z-10 -right-[7.75rem] -bottom-[12.5rem] md:-bottom-28"
+          loading="lazy"
         />
       </div>
     </main>
