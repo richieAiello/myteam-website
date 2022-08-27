26d54207fcb5bb226331962f3291a7f9bf2c630a
Richard Aiello <richaiello17@gmail.com>
2022-08-27 08:54:57 -0400

    Styles for HeroHome.jsx on tablet.

--- src/components/home/hero-section/HeroHome.jsx
+++ src/components/home/hero-section/HeroHome.jsx
@@ -2,12 +2,12 @@ import patternMobile from '../../../assets/bg-patterns/home-hero.svg';
 
 const HeroHome = props => {
   return (
-    <div className="bg-midnight-green pt-40">
-      <section className="container text-center relative pb-[12.3125rem]">
-        <h1 className="heading--hero mb-[1.125rem]">
+    <div className="bg-midnight-green pt-40 md:pt-56">
+      <section className="container flex flex-col items-center text-center relative pb-[12.3125rem] md:pb-64">
+        <h1 className="heading--hero mb-[1.125rem] md:max-w-[10ch] md:mb-6">
           Find the best <span className="text-coral">talent</span>
         </h1>
-        <p className="leading-7">
+        <p className="leading-7 md:max-w-[457px]">
           Finding the right people and building high performing teams
           can be hard. Most companies aren’t tapping into the
           abundance of global talent. We’re about to change that.
