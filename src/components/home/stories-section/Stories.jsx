fe97d316d22f45b09c69e55a7ccc4196093c94c5
Richard Aiello <richaiello17@gmail.com>
2022-08-27 13:05:37 -0400

    Styles for Stories.jsx on desktop.

--- src/components/home/stories-section/Stories.jsx
+++ src/components/home/stories-section/Stories.jsx
@@ -7,13 +7,13 @@ const Stories = props => {
   const data = storyData;
 
   return (
-    <div className="bg-jungle-green pt-[8.75rem] pb-[10.25rem] md:py-[6.25rem]">
+    <div className="bg-jungle-green pt-[8.75rem] pb-[10.25rem] md:py-[6.25rem] lg:py-[8.75rem]">
       <section className="container--secondary relative z-0 text-center">
-        <h2 className="heading max-w-[327px] mx-auto mb-12 md:max-w-full">
+        <h2 className="heading max-w-[327px] mx-auto mb-12 md:max-w-full lg:mb-14 lg:max-w-[32ch]">
           Delivering real results for top companies. Some of our
           <span className="text-rapture-blue"> success stories.</span>
         </h2>
-        <div className="grid gap-y-12">
+        <div className="grid gap-y-12 lg:grid-cols-3 lg:gap-x-[1.875rem]">
           {data.map(item => {
             return (
               <Story
@@ -29,13 +29,15 @@ const Stories = props => {
         <img
           src={patternTop}
           alt=""
-          className="absolute -z-10 -top-[8.75rem] -left-6 md:-top-[6.25rem] md:-left-[6.125rem]"
+          className="absolute -z-10 -top-[8.75rem] -left-6 md:-top-[6.25rem] md:-left-[6.125rem]
+          lg:-top-[8.75rem] lg:-left-[10.3125rem]"
           loading="lazy"
         />
         <img
           src={patternBottom}
           alt=""
-          className="absolute -z-10 -bottom-[10.25rem] -right-6 md:-bottom-[6.25rem] md:-right-[6.125rem]"
+          className="absolute -z-10 -bottom-[10.25rem] -right-6 md:-bottom-[6.25rem] md:-right-[6.125rem]
+          lg:-bottom-[8.75rem] lg:-right-[10.3125rem]"
           loading="lazy"
         />
       </section>
--- src/components/home/stories-section/Story.jsx
+++ src/components/home/stories-section/Story.jsx
@@ -1,11 +1,11 @@
 const Story = ({ quote, name, position, image }) => {
   return (
     <div className="pt-[2.25rem] bg-[url(./assets/icons/icon-quotes.svg)] bg-top bg-no-repeat">
-      <p className="mb-4">{quote}</p>
+      <p className="mb-4 lg:mb-6">{quote}</p>
       <h4 className="heading--sub mb-[0.125rem] text-rapture-blue">
         {name}
       </h4>
-      <p className="text-[0.8125rem] leading-[1.125rem] italic font-medium mb-4">
+      <p className="text-[0.8125rem] leading-[1.125rem] italic font-medium mb-4 lg:mb-8">
         {position}
       </p>
       <img
