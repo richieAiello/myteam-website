03812e98b76859052b527d5f9593330d13e0e9d3
Richard Aiello <richaiello17@gmail.com>
2022-08-27 14:09:42 -0400

    Styles for Contact.jsx on desktop.

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -5,20 +5,27 @@ import patternBottom from '../assets/bg-patterns/bg-pattern-contact-2.svg';
 
 const Contact = props => {
   return (
-    <main className="bg-midnight-green pt-40 pb-[5.5rem] overflow-hidden md:pt-56 md:pb-28">
-      <div className="container--secondary relative z-0 grid gap-y-14 md:gap-y-16">
+    <main
+      className="bg-midnight-green pt-40 pb-[5.5rem] overflow-hidden 
+      md:pt-56 md:pb-28 lg:pt-60 lg:pb-[7.5rem]"
+    >
+      <div
+        className="container--secondary relative z-0 grid gap-y-14 
+        md:gap-y-16 lg:grid-cols-2"
+      >
         <HeroContact />
         <ContactForm />
         <img
           src={patternTop}
           alt=""
-          className="hidden absolute -z-10 md:block md:-left-[12.375rem] md:top-0"
+          className="hidden absolute -z-10 md:block md:-left-[12.375rem] md:top-0 lg:-left-[16.5rem]"
           loading="lazy"
         />
         <img
           src={patternBottom}
           alt=""
-          className="absolute -z-10 -right-[7.75rem] -bottom-[12.5rem] md:-bottom-28"
+          className="absolute -z-10 -right-[7.75rem] -bottom-[12.5rem] md:-bottom-28 
+          lg:-right-[16.5rem] lg:-bottom-[7.5rem]"
           loading="lazy"
         />
       </div>
